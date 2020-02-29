e5ea15db2e20a5bcb8a019a98fc3b56e6a397ab3
ankitapal189 <ankitapal189@users.noreply.github.com>
2020-02-28 17:00:51 -0800

    add

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,26 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 hello <- function() {
   print("Hello, world!")
+}
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plus_two <- function(x)
+{
+  x+2
 }
