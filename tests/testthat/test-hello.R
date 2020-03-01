cfe9648c5f8c338495ecec09272b646edc81f960
ankitapal189 <ankitapal189@users.noreply.github.com>
2020-02-29 22:55:43 -0800

    update test

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -15,11 +15,3 @@ test_that("function works", {
   expect_equal(mult_two(9), 18)
 })
 
-test_that("function works", {
-  expect_equal(plus_two(4), 4)
-})
-
-test_that("function works", {
-  expect_equal(plus_two(6), 80)
-})
-
